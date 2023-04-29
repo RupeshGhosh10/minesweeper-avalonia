10958d3386f1c8e8940cb5cdbf2252f66ad41edc
RupeshGhosh10 <rupeshghosh10@gmail.com>
2023-04-29 19:36:20 +0530

    dynamically calculate height and width of board

--- Minesweeper/ViewModels/GameViewModel.cs
+++ Minesweeper/ViewModels/GameViewModel.cs
@@ -9,11 +9,19 @@ public partial class GameViewModel : ViewModelBase
 {
     public GameViewModel()
     {
-        Cells = GenerateCells(16, 16);
+        RowCount = 16;
+        ColumnCount = 16;
+        Cells = GenerateCells(RowCount, ColumnCount);
     }
 
     [ObservableProperty] 
     private ObservableCollection<Cell> _cells;
+
+    [ObservableProperty]
+    private int _rowCount;
+    
+    [ObservableProperty]
+    private int _columnCount;
     
     private static ObservableCollection<Cell> GenerateCells(int row, int column)
     {
