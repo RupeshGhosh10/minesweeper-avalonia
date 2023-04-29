b1873bdee659da09d0429268b05830e377064ec3
RupeshGhosh10 <rupeshghosh10@gmail.com>
2023-04-29 19:16:11 +0530

    add cells grid

---
+++ Minesweeper/Models/Cell.cs
@@ -0,0 +1,8 @@
+namespace Minesweeper.Models;
+
+public class Cell
+{
+    public int Column { get; set; }
+
+    public int Row { get; set; }
+}
--- Minesweeper/ViewModels/GameViewModel.cs
+++ Minesweeper/ViewModels/GameViewModel.cs
@@ -1,6 +1,31 @@
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using CommunityToolkit.Mvvm.ComponentModel;
+using Minesweeper.Models;
+
 namespace Minesweeper.ViewModels;
 
-public class GameViewModel : ViewModelBase
+public partial class GameViewModel : ViewModelBase
 {
+    public GameViewModel()
+    {
+        Cells = GenerateCells(16, 16);
+    }
+
+    [ObservableProperty] 
+    private ObservableCollection<Cell> _cells;
     
+    private static ObservableCollection<Cell> GenerateCells(int row, int column)
+    {
+        var cells = new ObservableCollection<Cell>();
+        for (var i = 0; i < row; i++)
+        {
+            for (var j = 0; j < column; j++)
+            {
+                cells.Add(new Cell { Row = i, Column = j });
+            }
+        }
+        
+        return cells;
+    }
 }
