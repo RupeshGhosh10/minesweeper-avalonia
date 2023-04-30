99dd53a09a27b4ed713a02bdd6a5d6600e9a95fd
RupeshGhosh10 <rupeshghosh10@gmail.com>
2023-05-01 00:56:00 +0530

    Distinguish cell left and right click

--- Minesweeper/Views/CellView.axaml.cs
+++ Minesweeper/Views/CellView.axaml.cs
@@ -1,4 +1,6 @@
 using Avalonia.Controls;
+using Avalonia.Input;
+using Avalonia.Interactivity;
 using Avalonia.Markup.Xaml;
 
 namespace Minesweeper.Views;
@@ -8,10 +10,21 @@ public partial class CellView : UserControl
     public CellView()
     {
         InitializeComponent();
+        var cellButton = this.FindControl<Button>("CellButton");
+        cellButton.AddHandler(PointerPressedEvent, CellButton_OnPointerPressed, RoutingStrategies.Tunnel);
     }
 
     private void InitializeComponent()
     {
         AvaloniaXamlLoader.Load(this);
+    }
+
+    private void CellButton_OnPointerPressed(object? sender, PointerPressedEventArgs e)
+    {
+        var point = e.GetCurrentPoint(sender as IInputElement);
+        if (point.Properties.IsRightButtonPressed)
+        {
+            
+        }
     }
 }
